8d80deef93213a17a78fe6aa77ed716ba656b084
Mohamud Olow <khalif18@live.com>
2019-08-04 08:11:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  constructor(props) {
+    super(props),
+      this.state = {
+      paras = 4,
+      html = true,
+      text = ''
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Sample Text Generator!
     </div>
   );
 }
