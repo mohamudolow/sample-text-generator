7c97bae46e059ad25d56f4638f0d20856364b899
Mohamud Olow <khalif18@live.com>
2019-08-05 10:53:47 -0700

    generate random text from the api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Output from './Components/Output';
 import axios from 'axios';
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      paras: 4,
-      html: true,
+      type: 'gibberish',
+      format: 'p',
+      num_elements: '5',
+      min_words: '40',
+      max_words: '100',
       text: ''
     };
   }
-  
+
   componentWillMount() {
     this.getSampleText();
   }
   getSampleText() {
-    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
-    .then((response) => {
-      this.setState({text: response.data.text}, function() {
+    axios.get('http://www.randomtext.me/api?'+this.state.type+'&'+this.state.format+'&-'+this.state.num_elements+'&'+this.state.min_words+'&-'+this.state.max_words, {crossdomain: true})
+      .then((res) => {
+      this.setState({
+        text: res.data.text_out}, function() {
         console.log(this.state);
       });
     })
-    .catch((err) => {
+      .catch((err) => {
       console.log(err);
     });
   }
   render() {
     return (
       <div className="App">
-      Sample Text Generator!
+      <Output value={this.state.text} />
       </div>
     );
   }
